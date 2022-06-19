1509a34f353bb0d195c5564b6f1f0e94b45b8780
KorolevAV <antoshka.992@mail.ru>
2022-06-19 13:42:53 +0300

    add check println for task003

--- task003.java
+++ task003.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 
 public class task003 {
     public static void main(String[] args) {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.println("Введите n: ");
-        int n = iScanner.nextInt();
-        iScanner.close();
+        Scanner sc = new Scanner(System.in);
+        int n;
+        do {
+            System.out.println("Введите положительное число n: ");
+            while (!sc.hasNextInt()) {
+                System.out.println("Введите положительное число n: ");
+                sc.next();
+            }
+            n = sc.nextInt();
+        } while (n <= 0);
+        sc.close();
         int answer = FindTriangularNumber(n);
         System.out.println(answer);
-    }
+        }
     public static int FindTriangularNumber(int a) {
         int res = a*(a+1)/2;
         return res;
